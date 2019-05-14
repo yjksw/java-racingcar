7c71ba12e1eb1db31e4b4f19872c5c3435667937
Doughnut K <48856468+kwanwookang@users.noreply.github.com>
2019-05-14 20:36:00 +0900

    [자동차 경주 게임] 도넛 재검사 (#59)
    * 계산, 덧셈 테스트
    
    * 차, 곱, 나누기 테스트
    
    * 복합 수식 테스트
    
    * 메인 로직
    
    * 리팩토링
    
    * 테스트 코드 작성
    
    * README.md 작성
    
    * 랜덤 넘버 생성, 자동차 객체, 자동차 이동
    
    * 계산기 리팩토링, 자동차 리팩토링
    
    * 입력 뷰 및 테스트
    
    * 가완성
    
    * 자동차 객체 테스트
    
    * 기능 목록 완성
    
    * 사용자 입력 리팩토링 및 테스트
    
    * 리팩토링 및 우승자 구하기 테스트
    
    * 최종 완성
    
    * 주석 작업
    
    * Update README.md
    
    * <REFACTOR> Added argument validation for Car object, extracted name validation method from multiple names validation method.
    
    * <REFACTOR> Final polishment.
    
    * <FIX> Input validation error fix.
    
    * <REFACTOR> Restructured Calculator App.
    
    * <FIX> Calculator token input error fix.
    
    * <FIX>Minor changes.
    
    * <REFACTOR> Last enhancement.
    
    * <REFACTOR> Exchanged the order of methods in Operator enum.
    
    * <FIX> I forgot to fix the bug that the winners are not printed in the same order as the user input.
    
    * <REFACTOR> Changed variable names to my taste.
    
    * <REFACTOR> Studied Java enum and refactored.
    
    * <FIX> Fixed string comparison with == instead of equals method.
    
    * <REFACTOR> Cleaner code
    
    * <REFACTOR> Even cleaner code
    
    * <REFACTOR> imma sleep
    
    * <REFACTOR> Simple name changes.
    
    * <FIX> Exception handling for divided by zero case.
    
    * Deleted all throws clauses
    
    * <REFACTOR> Coherence
    
    * <FIX> Tests and validation bug.
    
    * <FIX> Hangul test
    
    * <REFACTOR> Changed token list to queue.
    
    * <REFACTOR> Overall restructuring.
    
    * <REFACTOR> Separated race game running class and competitors info holding class.
    
    * <REFACTOR> Separated responsibilities.
    
    * <FIX> Input validation error fix.
    
    * <REFACTOR> Added iterable wrapper class.
    
    * .
    
    * <REFACTOR> Last one.
    
    * <FIX> Test fixed.
    
    * <REFACTOR> Changed method getResult to return Map instead of itself.
    
    * <REFACTOR> Deleted legacy files.
    
    * Update RaceResult.java

---
+++ src/main/java/racingcar/domain/RaceResult.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RaceResult {
+    private Map<String, Integer> cars;
+
+    public RaceResult(List<Car> cars) {
+        this.cars = Collections.unmodifiableMap(
+            new LinkedHashMap<String, Integer>() {{
+                cars.forEach(car -> put(car.toString(), car.getPosition()));
+            }}
+        );
+    }
+
+    public Map<String, Integer> getResult() {
+        return cars;
+    }
+
+    public Map<String, Integer> getWinners() {
+        int positionOfWinners = Collections.max(cars.values());
+        return Collections.unmodifiableMap(
+            new LinkedHashMap<String, Integer>() {{
+                    cars.entrySet().stream()
+                        .filter(car -> car.getValue() == positionOfWinners)
+                        .forEach(car -> put(car.getKey(), car.getValue()));
+            }}
+        );
+    }
+}
